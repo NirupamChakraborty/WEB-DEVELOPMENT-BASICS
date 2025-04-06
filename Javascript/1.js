176b6d80645811c2d8fea5a48e2693ebe7fa1fa4
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2025-04-06 21:42:37 +0530

    PASS

---
+++ Javascript/1.js
@@ -0,0 +1,23 @@
+/* In lecture 1 we read about console
+
+Data types in JS
+
+1. Primitive types
+   number, boolean, string, undefined, null, 
+   bigint,symbol ------> not used much
+  
+
+   use of typeof 456 
+  'number'
+
+  instagram follower/following  count(number)
+  follow -> yes or no (boolean)
+  text (string)
+
+  we learnt about NaN
+  0/0, nan-1, nan*1
+  nan + nan
+
+  type of nan = number
+ 
+*/
