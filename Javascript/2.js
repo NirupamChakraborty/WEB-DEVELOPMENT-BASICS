616f09b6232451defea14631ca32d3b0f910f5ca
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2025-04-12 12:59:51 +0530

    PASS

--- Javascript/2.js
+++ Javascript/2.js
@@ -24,11 +24,30 @@
 //  let up =msg.toUpperCase()
 //  console.log(up)
 
-// 3. methods with arguments
-// indexOf() methods -> returns first index of occurence of substring or letter
+// methods with arguments
+// 3. indexOf() methods -> returns first index of occurence of substring or letter
 let str ="IloveCodingv";
 str.indexOf("love") // 1
 str.indexOf("J") // -1 (not found)
 console.log(str.indexOf("v")) // 3
 
 // METHOD CHAINING 
+let msg ="    hello   "
+let nm = msg.trim().toUpperCase(); //left to right first trim then uppercase
+
+console.log(nm)
+// 4. slice(si, ei) ei is non included so ei +1
+str.slice(5);
+str.slice(1,5);
+// str.slice(-1) = str.slice(str.length-1) means last letter
+console.log(str.slice(-1))
+
+// 5. replace()-> replaces a value
+s1=str.replace("love","do")
+t1=str.replace("o","p") // replaces first occurance
+console.log(s1)
+console.log(t1)
+
+// 6. repeat() method returns a string with the number of copies of a string
+
+console.log(str.repeat(3))
