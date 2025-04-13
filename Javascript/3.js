2b3a1669990187476b54251822849310b09229a5
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2025-04-14 00:30:39 +0530

    PASS

--- Javascript/3.js
+++ Javascript/3.js
@@ -10,4 +10,36 @@ console.log(newArr);
 console.log(newArr[0][0]); // 0th letter at 0th index
 console.log(newArr[0].length)
 console.log(newArr.length)
+//ARRAYS ARE MUTABLE
+// ARRAY METHODS 
+// PUSH POP SHIFT UNSHIFT
+// 1. push
+let cars=["bmv", "porsche","lambo","toyota"];
+cars.push("pagani");
+cars.push("proton");
+console.log(cars);
+cars.push("mahindra");
+cars.push("tata");
+// 2. pop
+console.log(cars.pop()); //tata 
+console.log(cars)
+// 3. unshift -> add at first 
+cars.unshift("maruti");
+console.log(cars);
+// 4. Shift
+// cars.shift();
+console.log(cars.shift())
+console.log(cars);
+// 5. indexof
+a=cars.indexOf("porsche");
+console.log(a);
+//6. includes
+aaa = cars.includes("porsche");
+console.log(aaa);
+// 7. concatenate
+v8engine =["porsche", "supra", "land cruser"];
+let ap=cars.concat(v8engine)
+console.log(ap)
+// 8. reverse
+console.log(cars.reverse());
 
