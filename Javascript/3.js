4c681446e29cbeed509b3d7e63c94f75832098b2
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2025-04-13 22:38:37 +0530

    PASS

---
+++ Javascript/3.js
@@ -0,0 +1,13 @@
+// ARRAYS IN JS (DATA STRUTURE)
+let students = ["aman", "shradha", "rajat"];
+console.log(students);
+typeof students;
+let info=["aman", 25, 7.569]; //mixed array
+let newArr = [];
+newArr[0] = "aman";
+newArr[1] = 100;
+console.log(newArr);
+console.log(newArr[0][0]); // 0th letter at 0th index
+console.log(newArr[0].length)
+console.log(newArr.length)
+
