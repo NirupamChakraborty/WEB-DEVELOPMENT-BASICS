80367e0f4b4db07f9b0559f9caee1f2d77f9997e
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2025-04-19 18:58:18 +0530

    PASS

--- Javascript/4.js
+++ Javascript/4.js
@@ -1,9 +1,17 @@
 // MOVIES PASS
-let favmovie = "avatar";
-let guess = prompt("guess the favmovie");
-while ((guess!=favmovie) && (guess!="quit")) {
-    guess=prompt("guess ")  
-}
-if(guess==favmovie){
-    alert("congrats")
+// let favmovie = "avatar";
+// let guess = prompt("guess the favmovie");
+// while ((guess!=favmovie) && (guess!="quit")) {
+//     guess=prompt("guess ")  
+// }
+// if(guess==favmovie){
+//     alert("congrats")
+// }
+let heros =[["marcos","garud","paracamando"], ["army", "air","navy"]];
+for (let i = 0; i < heros.length; i++) {
+    console.log(i, heros[i], heros[i].length);
+    for( let j =0; j< heros[i].length; j++){
+        console.log(`j=${j}, ${heros[i][j]}`);
+    }
+    
 }
