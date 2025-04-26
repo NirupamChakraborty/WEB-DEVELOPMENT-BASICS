0e59e5168998c0bc780c0639c1c910d9704547d3
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2025-04-26 22:14:11 +0530

    PASS

---
+++ Javascript/5.js
@@ -0,0 +1,32 @@
+//function defination
+function funcName(){
+    // do something
+}
+
+// function calling
+// funcName();j
+function hello(){
+    console.log("hello");
+
+}
+hello();
+
+// function to roll a dice
+function printDice(){
+    let dice = Math.floor(Math.random()*6) +1;
+}
+
+printDice()
+// arguments
+// pass
+
+function printInfo(name, age){
+    console.log(`${name}'s age is${age}`); 
+}
+printInfo("Aman", 28);
+printInfo("Pratik", 38);
+printInfo("P", 38);
+printInfo("A", 38);
+printInfo("S", 38);
+printInfo("S", 38);
+
