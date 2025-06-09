0612a9f661b62f5b83518d4d23010422cbac5e30
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2025-06-09 10:58:51 +0530

    PASS

--- Javascript/6.js
+++ Javascript/6.js
@@ -26,4 +26,49 @@ console.log("hi before")
 setTimeout(()=>{
     console.log("hi after 4567ms")
 }, 4567);
-console.log("HI after")
+console.log("HI after");
+
+
+// SET INTERVAL -> keeps executing something after fixed interval
+setInterval(()=>{
+    console.log("hi after 4567ms")
+}, 4567);
+
+// to stop
+const id = setInterval(()=>{
+    console.log("hi after 4567ms")
+}, 4567);
+
+clearInterval(id);
+
+// this with arrow functions
+
+// ARROW-> lexical scope -> parent call scope 
+// func -> scope ->this-> calling object
+const student={
+    name:"AMAN",
+    location: "delhi",
+    prop: this, //global
+    getName: function () {
+        console.log(this); //student scope
+        return this.name;
+    },
+    getLocation: ()=>{
+        console.log(this) // parent's scope
+        return this.location;
+    },
+};
+const a =567; // global scope
+student.getName;
+student.getLocation;
+
+const sq =(n)=>n*n;
+console.log(sq(4567));
+
+let id1= setInterval(()=>{
+    console.log("hello world");
+},3000);
+
+setTimeout(()=>{
+    clearInterval(id);
+},15000)
