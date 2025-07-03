38db8122fec7cfc6d001b379cd5e1f2955f05896
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2025-07-03 14:44:46 +0530

    PASS

--- Javascript/JS DOM/Practice/1_Todo.js
+++ Javascript/JS DOM/Practice/1_Todo.js
@@ -1,14 +1,50 @@
 let btn = document.querySelector("button");
 let input = document.querySelector("input");
 let ul = document.querySelector("ul");
+
 // event listener
 btn.addEventListener("click",()=>{
+    if (input.value=="") {
+        return;
+    }
     let item = document.createElement("li");
     item.innerText = input.value;
+
+
     ul.appendChild(item);
+
     // console.log(input.value);
     //reset
     input.value ="";
-})
 
+
+    // delete button
+    let dltBtn = document.createElement("button");
+    dltBtn.innerText="delete";
+    item.appendChild(dltBtn);
+    dltBtn.classList.add("delete");
+
+});
+
+
+// this doesnot apply to newly created elements 
+// let dltbtns = document.querySelectorAll(".delete");
+// for(dltBtn of dltbtns){
+//     dltBtn.addEventListener("click",()=>{
+//         // we have delete the parent i.e. li item
+//         let par = this.parentElement;
+//         par.remove();
+//     });
+// }
+
+// so to do this we use event delegation or event bubling 
+// that means apply logic for parent instead of child 
+
+ul.addEventListener("click", (event)=>{
+    if(event.target.nodeName=="BUTTON") {
+        let itemList = event.target.parentElement;
+        itemList.remove();
+    }
+    
+})
 
