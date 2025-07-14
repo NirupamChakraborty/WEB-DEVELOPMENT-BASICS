a4dd8e19e7ab6a682da735771bf72ee5c080efcc
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2025-07-14 23:46:35 +0530

    PASS

---
+++ 1.Express/EJSDIR/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req,res)=>{
+    // res.send("this is home")
+    res.render("home.ejs")
+})
+
+app.listen(port, ()=>{
+    console.log("app is listening")
+})
