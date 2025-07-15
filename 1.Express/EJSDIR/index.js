d95e92aedffad49e36a8e3597f8f8204f7968074
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2025-07-15 14:04:40 +0530

    PASS

--- 1.Express/EJSDIR/index.js
+++ 1.Express/EJSDIR/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const path = require("path")
+
+const port = 8080; 
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"/ views"))
 
 app.get("/", (req,res)=>{
     // res.send("this is home")
-    res.render("home.ejs")
+    // res.render("home.ejs")
+})
+
+app.get("/rolldice", (req,res)=>{
+    let diceValue =Math.floor(Math.random()*6) +1
+    res.render("rolldice.ejs", {num:diceValue})
 })
 
 app.listen(port, ()=>{
